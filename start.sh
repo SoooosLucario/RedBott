0b4e78d96e3079a84936f1bb98495e139251a421
SoooosLucario <44749620+SoooosLucario@users.noreply.github.com>
2018-11-04 17:49:19 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 # For development use (simple logging, etc):
-python server.py
+python server.py &
+python3.5 red.py --debug --co-owner 375025650542706688
 # For production use: 
 # gunicorn server:app -w 1 --log-file -
